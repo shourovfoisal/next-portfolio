8dc28188b9ca6f999d27644a39cde8b9bbe7cfa0
Shourov Foisal <shourovfoisal@gmail.com>
2022-11-30 22:58:19 +0600

    Polished the top navigation menu.
    Added active link styles feature.
    Beautified the search box.
    Installed react-icons.
    Added clickable search icon.

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import logo from "../public/logo.png";
 import styles from "./TopNav.module.scss";
+import { IoSearch } from "react-icons/io5";
 
 const TopNav = () => {
+	const [link, setLink] = useState(0);
+
+	const router = useRouter();
+
+	useEffect(() => {
+		if (router.pathname === "/") setLink(1);
+		if (router.pathname === "/about") setLink(2);
+		if (router.pathname === "/contact") setLink(3);
+	}, [router.pathname]);
+
+	const activeLinkStyles = {
+		color: "#22b14c",
+		borderBottom: "2px solid #22b14c",
+	};
+
 	return (
 		<div className={styles.navContainer}>
 			<div className="logo">
@@ -15,15 +33,35 @@ const TopNav = () => {
 					/>
 				</Link>
 			</div>
+
 			<div className={styles.navMenu}>
-				<Link href="/">Home</Link>
-				<Link href="/about">About</Link>
-				<Link href="/contact">Contact</Link>
+				<Link
+					href="/"
+					style={link === 1 ? activeLinkStyles : {}}
+				>
+					Home
+				</Link>
+				<Link
+					href="/about"
+					style={link === 2 ? activeLinkStyles : {}}
+				>
+					About
+				</Link>
+				<Link
+					href="/contact"
+					style={link === 3 ? activeLinkStyles : {}}
+				>
+					Contact
+				</Link>
 			</div>
+
 			<div className={styles.searchBox}>
 				<input
 					type="text"
 					placeholder="Search something."
+				/>
+				<IoSearch
+					style={{ cursor: "pointer", color: "#22b14c", fontSize: "22px" }}
 				/>
 			</div>
 		</div>
