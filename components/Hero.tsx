84b1cce3a939db51196b5d962d7273604aba0357
Shourov Foisal <shourovfoisal@gmail.com>
2022-12-24 18:32:32 +0600

    Added stats and scroll decoration
    Added my status below the hero button.
    Removed border from the secondary button. Added icon to it.
    Added a scroll decoration to the left of the hero section.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,28 +1,53 @@
 // import Image from "next/image";
 // import myImage from "../public/images/myImage.jpg";
+import Link from "next/link";
 import styles from "./Hero.module.scss";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
 	return (
 		<div className={styles.heroContainer}>
-			{/* <div className={styles.heroImageAndText}> */}
-				<div className={styles.heroTextBox}>
-					<h1>
-						Shourov <span>Foisal</span>
-					</h1>
-					<p>Full-Stack Web Developer</p>
-					<div className={styles.heroButtons}>
+			<div className={styles.scrollReminder}>
+				<div className={styles.scrollDashOne}></div>
+				<div className={styles.scrollDashTwo}></div>
+				
+				<div className={styles.scrollText}>
+					Scroll Down
+				</div>
+			</div>
+			<div className={styles.heroTextBox}>
+				<h1>
+					Shourov <span>Foisal</span>
+				</h1>
+				<p>Full-Stack Web Developer</p>
+				<div className={styles.heroButtons}>
+					<Link href="resume">
 						<button>View Resume</button>
-						<button>Check My Works</button>
+					</Link>
+					<Link href="works">
+						<button>Check My Works <span><FaLocationArrow /></span></button>
+					</Link>
+				</div>
+				
+				<div className={styles.heroStats}>
+					<h2>&#8212; My Recent Stats</h2>
+					
+					<div className={styles.singleStats}>
+						<h3>Projects</h3>
+						<p>15+</p>
+					</div>
+					
+					<div className={styles.singleStats}>
+						<h3>Large Scale Projects</h3>
+						<p>4+</p>
+					</div>
+					
+					<div className={styles.singleStats}>
+						<h3>Satisfied Clients</h3>
+						<p>9+</p>
 					</div>
 				</div>
-				{/* <div className={styles.heroImageBoxContainer}>
-					<div className={styles.heroImageContainer}>
-						<Image src={myImage} alt="" priority />
-					</div>
-				</div> */}
-			{/* </div> */}
-			<div></div>
+			</div>
 		</div>
 	);
 };
